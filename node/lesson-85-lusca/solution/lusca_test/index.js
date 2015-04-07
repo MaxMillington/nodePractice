2649a702498825157e3f293d73961acea2c3c373
Clay Boyd <clboyd@paypal.com>
2015-04-07 06:57:23 +0530

    Typos from teaching the class.

--- node/lesson-85-lusca/solution/lusca_test/index.js
+++ node/lesson-85-lusca/solution/lusca_test/index.js
@@ -14,7 +14,6 @@ var kraken = require('kraken-js'),
 
 app.use(kraken(options));
 
-
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
 });
