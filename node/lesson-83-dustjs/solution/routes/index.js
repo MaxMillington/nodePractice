381b69e9b2e5eade986d7656d8dc591e0133c7d6
Clay Boyd <clboyd@paypal.com>
2015-10-01 14:54:58 -0500

    updated dust labs

--- node/lesson-83-dustjs/solution/routes/index.js
+++ node/lesson-83-dustjs/solution/routes/index.js
@@ -2,8 +2,16 @@
 
 module.exports = function module(app) {
 
-  var controller = require('./controller')(app);
+    var controller = require('./controller')(app);
 
-  app.get('/', controller.display);
-  
+    app.get('/', controller.display);
+
+    app.post('/submitAssessment', function (req, res, next) {
+        var model = {
+            body: req.body,
+            keys: Object.keys(req.body)
+        };
+        res.render('templates/results', model);
+    });
+
 };
