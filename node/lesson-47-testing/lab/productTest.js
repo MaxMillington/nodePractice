3f3a7b2e2251a36c12958a24447eaf10711486f5
Max Millington <mmillington@paypal.com>
2018-01-08 14:17:22 -0800

    add test

--- node/lesson-47-testing/lab/productTest.js
+++ node/lesson-47-testing/lab/productTest.js
@@ -5,4 +5,16 @@ var Product = require('./product');
 var product = new Product();
 
 suite('product suite', function productSuite() {
+  test('add and delete', function() {
+    product.add('party')
+    assert.equal(product.getCount(), 1)
+    product.deleteAll()
+    assert.equal(product.getCount(), 0)
+  })
+  test('doAsyncTest', function doAsyncTest(done) {
+    product.doAsync(function callback(value) {
+      assert.equal(value, true)
+      done()
+    })
+  })
 });
